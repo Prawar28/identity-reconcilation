7fbc16564f3f0eac0d41c1c652ce7977ebdf8c56
Prawar28 <53871904+Prawar28@users.noreply.github.com>
2024-05-28 21:54:36 +0530

    Update config.js - add database

--- config/config.js
+++ config/config.js
@@ -10,6 +10,7 @@ module.exports = {
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
+    "database": process.env.DATABASE,
     "port": process.env.DB_PORT,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
